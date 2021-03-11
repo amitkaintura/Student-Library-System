149e8b876465f7404e1274b43be29d4000b5074e
amit <amitkaintura3@gmail.com>
2021-03-11 11:21:01 +0530

    initial commit

---
+++ Student_Library_System.py
@@ -0,0 +1,55 @@
+class Library:
+    def __init__(self, listOfBooks):
+        self.books = listOfBooks
+
+    def displayAvailBooks(self):
+        print("Books present in this libray are:")
+        for book in self.books:
+            print("\t*", book)
+    def borrowBook(self,bookName):
+        if bookName in self.books:
+            print(f"You have been issued {bookName}. Please keep it safe")
+            self.books.remove(bookName)
+        else:
+            print("Sorry, this book is not available")
+            return False
+
+    def returnBook(self,bookName):
+        self.books.append(bookName)
+        print("Thanks for returning this book")
+
+class Student:
+    def requestBook(self):
+        self.book = input("Enter the name of Book you want to borrow : ")
+        return self.book
+
+    def returnBook(self):
+        self.book = input("Enter the book of name you want to return")
+        return self.book
+        
+
+if __name__ =="__main__":
+    collegeLibrary = Library(["Python", "Django", "Git", "Rest"])
+    student = Student()
+    while(True):
+        welcomeMsg = """
+=====Welcome to our Library ======
+    Please choose an option:
+        1. List all Books
+        2. Request a book
+        3. Add/Return a book
+        4. Exit the Library
+        """
+        print(welcomeMsg)
+        a = int(input("Enter a choice - "))
+        if a ==1:
+            collegeLibrary.displayAvailBooks()
+        elif a == 2:
+            collegeLibrary.borrowBook(student.requestBook())
+        elif a ==3:
+            collegeLibrary.returnBook(student.returnBook())
+        elif a == 4:
+            print("Thanks for choosing Library")
+            exit()
+        else:
+            print("invalid choice")
